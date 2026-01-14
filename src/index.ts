1f9690ddd488f25a279441374b10818a18b1fefe
Ivan Barlog <ivan.barlog@beesolve.com>
2026-01-14 20:55:20 +0100

    feat: upgrade bun runtime

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 import { type NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
-const bunVersion = "1.3.5";
+const bunVersion = "1.3.6";
 
 export interface BunFunctionProps {
   /**
